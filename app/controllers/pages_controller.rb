3ca97ff4bdd1a6f5ff24ee3ba73ff1fb0e39f90a
Bill <billhewitt22@googlemail.com>
2017-12-05 13:13:33 +0000

    ready for heroku

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,83 @@
+class PagesController < ApplicationController
+  
+  def home
+  end
+
+  def engagement
+  end
+
+  def quality
+  end
+
+  def teamwork
+  end
+
+  def reward
+  end
+
+  def training
+  end
+
+  def opportunities
+  end
+
+  def organisation
+  end
+
+  def mywork
+  end
+
+  def management
+  end
+
+  def quality
+  end
+
+  def leadership
+  end
+
+  def communication
+  end
+
+  def survey
+  end
+
+  def stats
+  end
+
+  def action
+  end
+
+  def users
+  end
+ 
+  def teams
+  end 
+
+  def settings
+  end 
+
+  def settings1
+  end 
+
+  def settings2
+  end 
+
+  def profile
+  end 
+
+  def profile
+  end 
+
+  def usersnew
+  end
+
+  def teamsnew
+  end 
+
+  def compare
+  end 
+
+  def actionpreview
+  end 
+end
---
+++ config/routes.rb
@@ -0,0 +1,55 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+
+  get 'pages/engagement'
+
+  get 'pages/quality'
+
+  get 'pages/opportunities'
+
+  get 'pages/teamwork'
+
+  get 'pages/reward'
+
+  get 'pages/training'
+
+  get 'pages/organisation'
+
+  get 'pages/mywork'
+
+  get 'pages/management'
+
+  get 'pages/quality'
+
+  get 'pages/leadership'
+
+  get 'pages/communication'
+
+  get 'pages/survey'
+
+  get 'pages/stats'
+
+  get 'pages/action'
+
+  get 'pages/users'
+
+  get 'pages/teams'
+
+  get 'pages/settings'
+
+  get 'pages/settings1'
+
+  get 'pages/settings2'
+
+  get 'pages/profile'
+
+  get 'pages/usersnew'
+
+  get 'pages/teamsnew'
+
+  get 'pages/compare'
+  
+  get 'pages/actionpreview'
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
